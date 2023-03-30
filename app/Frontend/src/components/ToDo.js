f2a0fa25146cb433ab9f742c31aa9c8337915dbf
zaferdrn0 <zaaduu@outlook.com>
2023-03-30 22:50:18 +0300

    heyyo

--- app/Frontend/src/components/ToDo.js
+++ app/Frontend/src/components/ToDo.js
@@ -1,43 +1,45 @@
-import React, { useState } from 'react'
-import "./ToDo.css"
-const ToDo = () => {
-    const [todoType, setTodoType] = useState([{type:"okul"}, {type:"is"},{type:"gunluk"}])
-    const [todo, setTodo] = useState([])
-    const [inpValue, setInpValue] = useState("")
+import React, { useState } from "react";
+import "./ToDo.css";
 
-    const InputChange = (event) =>{
-      setInpValue(event.target.value);
-    }
-    const addToDo = (event) =>{
+const ToDo = (props) => {
+  const { label } = props;
 
-      setTodo([...todo, {text:inpValue}])
-      setInpValue("");
-    }
+  const [todo, setTodo] = useState([]);
+  const [inpValue, setInpValue] = useState("");
 
-    const deleteToDo = (i) =>{
-      setTodo(todo.filter((_, index) => index !== i));
-    }
+  const InputChange = (event) => {
+    setInpValue(event.target.value);
+  };
+  const addToDo = (event) => {
+    setTodo([...todo, { text: inpValue }]);
+    setInpValue("");
+  };
+
+  const deleteToDo = (i) => {
+    setTodo(todo.filter((_, index) => index !== i));
+  };
+
   return (
-    <div className='home'>
-        <div className='container'>
-          <div className='todo-app'>
-           {todoType.map((type,i) =>(
-           <>
-                   
-                <div key={i} className='input'>
-                    <h2 >{type.type}</h2> 
-                    <input type="text" value ={inpValue} onChange = {InputChange}/>
-                    <button onClick={addToDo}>Add To Do</button>
-                </div>
-                <div className='todo-list'>{todo.map((to,i) =>(<><p key={i}>{to.text}
-                     </p> <button onClick={() => deleteToDo(i)}>X</button></>))}
-                </div> 
-            </> ))}
-            
+    <div className="home">
+      <div className="container">
+        <div className="todo-app">
+          <div className="input">
+            <h2>{label}</h2>
+            <input type="text" value={inpValue} onChange={InputChange} />
+            <button onClick={addToDo}>Add To Do</button>
+          </div>
+          <div className="todo-list">
+            {todo.map((to, i) => (
+              <>
+                <p key={i}>{to.text}</p>{" "}
+                <button onClick={() => deleteToDo(i)}>X</button>
+              </>
+            ))}
           </div>
         </div>
-       </div>
-  )
-}
+      </div>
+    </div>
+  );
+};
 
-export default ToDo
+export default ToDo;
--- app/Frontend/src/page/Home.js
+++ app/Frontend/src/page/Home.js
@@ -1,17 +1,27 @@
-import Footer from '../components/Footer'
-import Header from '../components/Header'
-import ToDo from '../components/ToDo'
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import ToDo from "../components/ToDo";
 
 const Home = () => {
-    
-    
   return (
     <div>
-       <div><Header/></div> 
-       <div><ToDo/></div>
-        <div><Footer/></div>
-   </div>
-  )
-}
+      <div>
+        <Header />
+      </div>
+      <div>
+        <ToDo label="okul" />
+      </div>
+      <div>
+        <ToDo label="is" />
+      </div>
+      <div>
+        <ToDo label="gunluk" />
+      </div>
+      <div>
+        <Footer />
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
