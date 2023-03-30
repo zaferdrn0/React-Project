cde328c5e43b41f71859c9372ddaa9a2979b17c4
zaferdrn0 <zaaduu@outlook.com>
2023-03-30 15:21:18 +0300

    push

--- src/page/Register.js
+++ src/page/Register.js
@@ -1,15 +1,36 @@
 import React from 'react'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
+import "./css/Register.css"
 
 const Register = () => {
   return (
     <>
     <div><Header/></div>
-    <h1>    Register burası</h1>
+        <div className='register'>
+            <div className='container'>
+                <div className='screen'>
+                    <div className='logo'>
+                        <img src='/images/logo.png'></img>
+                    </div>
+                    <div className='input'>
+                        <input type="text" placeholder='Username' />
+                        <input type="text" placeholder='Email' />
+                        <input type="text" placeholder='Password' />
+                    </div>
+
+                    <div className='login-btn'><button>REGISTER IN NOW</button></div>
+
+                </div>
+
+                   
+            </div>
+        </div>
     <div><Footer/></div>
     </>
   )
 }
 
 export default Register
+
+//background: linear-gradient(90deg, #5D54A4, #7C78B8);		
