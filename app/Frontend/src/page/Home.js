9f5fcab89e06df96ff166a8b155078259e9b972f
zaferdrn0 <zaaduu@outlook.com>
2023-03-30 22:34:39 +0300

    heyyo

--- app/Frontend/src/page/Home.js
+++ app/Frontend/src/page/Home.js
@@ -1,39 +1,14 @@
-import React, { useState } from 'react'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
+import ToDo from '../components/ToDo'
 
 const Home = () => {
     
-    const [todo, setTodo] = useState([])
-    const [inpValue, setInpValue] = useState("")
-
-    const InputChange = (event) =>{
-      setInpValue(event.target.value);
-    }
-    const addToDo = (event) =>{
-
-      setTodo([...todo, {text:inpValue}])
-      setInpValue("");
-    }
-
-    const deleteToDo = (i) =>{
-      setTodo(todo.filter((_, index) => index !== i));
-    }
-
+    
   return (
     <div>
        <div><Header/></div> 
-       <div className='home'>
-        <div className='container'>
-          <div className='todo-app'>
-            <div className='input'>
-              <input type="text" value ={inpValue} onChange = {InputChange}/>
-              <button onClick={addToDo}>Add To Do</button>
-            </div>
-            <div className='toDo-list'>{todo.map((to,i) =>(<><p key={i}>{to.text}</p> <button onClick={() => deleteToDo(i)}>X</button></>))}</div>
-          </div>
-        </div>
-       </div>
+       <div><ToDo/></div>
         <div><Footer/></div>
    </div>
   )
