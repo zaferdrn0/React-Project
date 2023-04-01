be95269c99de1aa7e0b4f9a905d69cc656dfdb05
zaferdrn0 <zaaduu@outlook.com>
2023-04-01 16:21:29 +0300

    update

--- app/Frontend/src/page/Login.js
+++ app/Frontend/src/page/Login.js
@@ -1,67 +1,84 @@
-import React, { useState } from 'react'
-import Footer from '../components/Footer'
-import Header from '../components/Header'
-import "./css/Register.css"
+import React, { useState } from "react";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import "./css/Register.css";
 const Login = () => {
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [loading, setLoading] = useState(false);
-    const [isDisabled, setIsDisabled] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [isDisabled, setIsDisabled] = useState(false);
+  const [message, setMessage] = useState("");
 
-    const userLogin = async (email,password) =>{
-      setLoading(true)
-      setIsDisabled(true)
-        
-  const response = await fetch('http://localhost:3001/login', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ email:email, password:password }),
-    credentials: 'include',
-  });
+  const userLogin = async (email, password) => {
+    setLoading(true);
+    setIsDisabled(true);
 
-  const data = await response.json({
-   
-  }).then((data) => {
-    if(data.data ==="1"){
-      setLoading(false)
-      setIsDisabled(false)
-    }
-  })
+    const response = await fetch("http://localhost:3001/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email: email, password: password }),
+      credentials: "include",
+    });
 
-  return data;
-    }
+    const data = await response.json({}).then((data) => {
+      let messages = data.message;
+      setMessage(messages);
+      if (data.data === "1") {
+        setLoading(false);
+        setIsDisabled(false);
+      }
+    });
 
-    if(loading === true){
-      
-    }
+    return data;
+  };
 
-
-
+  if (loading === true) {
+  }
 
   return (
     <>
-    <div><Header giris= "Home" url ="/"/></div>
-        <div className='register'>
-            <div className='container'>
-                <div className='screen'>
-                    <div className='logo'>
-                        <img src='/images/logo.png' alt=""></img>
-                    </div>
-                    <div className='input'>
-                        <input disabled={isDisabled} value={email} type="text" placeholder='Email' onChange={(event) =>setEmail(event.target.value)} />
-                        <input disabled={isDisabled} value={password} type="password" placeholder='Password' onChange={(event) => setPassword(event.target.value)} />
-                    </div>
-
-                    <div className='login-btn'><button disabled={isDisabled} onClick={() =>userLogin(email,password)}>LOGIN NOW</button></div>
-                    {loading && <div>Loading...</div>}
-                </div>
-
-                   
+      <div>
+        <Header giris="Home" url="/" />
+      </div>
+      <div className="register">
+        <div className="container">
+          <div className="screen">
+            <div className="logo">
+              <img src="/images/logo.png" alt=""></img>
             </div>
+            <div className="input">
+              <input
+                disabled={isDisabled}
+                value={email}
+                type="text"
+                placeholder="Email"
+                onChange={(event) => setEmail(event.target.value)}
+              />
+              <input
+                disabled={isDisabled}
+                value={password}
+                type="password"
+                placeholder="Password"
+                onChange={(event) => setPassword(event.target.value)}
+              />
+            </div>
+            <div className="login-btn">
+              <button
+                disabled={isDisabled}
+                onClick={() => userLogin(email, password)}
+              >
+                LOGIN NOW
+              </button>
+            </div>
+            {loading && <div>Loading...</div>} <div><br></br>{message}</div>
+          </div>
         </div>
-    <div><Footer/></div>
+      </div>
+      <div>
+        <Footer />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- app/Frontend/src/page/Register.js
+++ app/Frontend/src/page/Register.js
@@ -1,67 +1,96 @@
-import React, { useState } from 'react'
-import Footer from '../components/Footer'
-import Header from '../components/Header'
-import "./css/Register.css"
+import React, { useState } from "react";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import "./css/Register.css";
 
 const Register = () => {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [isdisabled, setIsDisabled] = useState(false);
+  const [message, setMessage] = useState("");
 
-    const [email,setEmail] = useState("")
-    const [username, setUsername] = useState("")
-    const [password, setPassword] = useState("")
-    const [loading, setLoading] = useState(false)
-    const [isdisabled,setIsDisabled] = useState(false)
+  const userRegister = async (email, password, username) => {
+    setLoading(true);
+    setIsDisabled(true);
+    const response = await fetch("http://localhost:3001/register", {
+      method: "POST",
 
-    const userRegister = async (email,password,username) =>{
-        setLoading(true)
-        setIsDisabled(true)
-  const response = await fetch('http://localhost:3001/register', {
-    method: 'POST',
-    
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ email:email, password:password, username:username })
-  });
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+        username: username,
+      }),
+    });
 
-  const data = await response.json({
-   
-  }).then((data) => {
-     if(data.data === "1"){
-      setLoading(false)
-      setIsDisabled(true)
-     }
-  })
+    const data = await response.json({}).then((data) => {
+      let messages = data.message;
+      setMessage(messages);
+      if (data.data === "1") {
+        setLoading(false);
+        setIsDisabled(true);
+      }
+    });
 
-  return data;
-    }
-
+    return data;
+  };
 
   return (
     <>
-    <div><Header giris= "Home" url ="/"/></div>
-        <div className='register'>
-            <div className='container'>
-                <div className='screen'>
-                    <div className='logo'>
-                        <img src='/images/logo.png' alt=""></img>
-                    </div>
-                    <div className='input'>
-                        <input disabled ={isdisabled} value={username} type="text" placeholder='Username' onChange={(event) =>setUsername(event.target.value)} />
-                        <input disabled ={isdisabled} value={email} type="text" placeholder='Email' onChange={(event) =>setEmail(event.target.value)}/>
-                        <input disabled ={isdisabled} value={password} type="password" placeholder='Password' onChange={(event) =>setPassword(event.target.value)}/>
-
-                    </div>
-
-                    <div className='login-btn'><button disabled ={isdisabled} onClick={() => userRegister(email,password,username)}>REGISTER IN NOW</button></div>
-                    {loading && <div>Loading...</div>}
-                </div>
-
-                   
+      <div>
+        <Header giris="Home" url="/" />
+      </div>
+      <div className="register">
+        <div className="container">
+          <div className="screen">
+            <div className="logo">
+              <img src="/images/logo.png" alt=""></img>
             </div>
+            <div className="input">
+              <input
+                disabled={isdisabled}
+                value={username}
+                type="text"
+                placeholder="Username"
+                onChange={(event) => setUsername(event.target.value)}
+              />
+              <input
+                disabled={isdisabled}
+                value={email}
+                type="text"
+                placeholder="Email"
+                onChange={(event) => setEmail(event.target.value)}
+              />
+              <input
+                disabled={isdisabled}
+                value={password}
+                type="password"
+                placeholder="Password"
+                onChange={(event) => setPassword(event.target.value)}
+              />
+            </div>
+            <div className="login-btn">
+              <button
+                disabled={isdisabled}
+                onClick={() => userRegister(email, password, username)}
+              >
+                REGISTER IN NOW
+              </button>
+            </div>
+            {loading && <div>Loading...</div>}
+            <br></br> <div>{message}</div>
+          </div>
         </div>
-    <div><Footer/></div>
+      </div>
+      <div>
+        <Footer />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
 
-//background: linear-gradient(90deg, #5D54A4, #7C78B8);		
+//background: linear-gradient(90deg, #5D54A4, #7C78B8);
